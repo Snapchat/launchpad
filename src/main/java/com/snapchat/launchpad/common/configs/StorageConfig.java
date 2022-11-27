b51f09bc56adc9624dc5303797653eff43181560
Lunan Li <lli3@snapchat.com>
2022-11-26 18:46:41 -0800

    fix

--- src/main/java/com/snapchat/launchpad/common/configs/StorageConfig.java
+++ src/main/java/com/snapchat/launchpad/common/configs/StorageConfig.java
@@ -3,12 +3,14 @@ package com.snapchat.launchpad.common.configs;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Profile("conversion-log")
 @Configuration
 @ConfigurationProperties("storage-config")
+@EnableConfigurationProperties
 public class StorageConfig {
     @JsonProperty("storage-prefix")
     private String storagePrefix;
